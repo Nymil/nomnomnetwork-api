510ea54a4b91bdfe11d3f11fae623ebf3d98516f
Mattice Heyzak <mattice.heyzak@student.howest.be>
2024-06-30 20:57:37 +0200

    adding in user service

--- app/Modules/Users/Services/UserService.php
+++ app/Modules/Users/Services/UserService.php
@@ -4,12 +4,31 @@ namespace App\Modules\Users\Services;
 
 use App\Models\User;
 use App\Modules\Core\Services\Service;
+use Illuminate\Support\Facades\Hash;
 
 class UserService extends Service {
+
+    protected $rules = [
+        "add" => [
+            'name' => 'required|unique:users,name',
+            'password' => 'required|min:6',
+        ],
+    ];
 
     public function __construct(User $model)
     {
         parent::__construct($model);
+    }
+
+    public function add($data, String $ruleKey = "add") {
+        if (!$this->validate($data, $ruleKey)) {
+            return;
+        }
+
+        $user = new User();
+        $user->name = $data['name'];
+        $user->password = Hash::make($data['password']);
+        $user->save();
     }
 
 }
