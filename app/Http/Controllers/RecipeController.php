ec40e8b33e581bd18cf9df5125c72e02cfe76f80
Mattice Heyzak <mattice.heyzak@student.howest.be>
2024-07-01 10:51:25 +0200

    controller constructors

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Modules\Recipes\Services\RecipeService;
 use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
-    //
+    protected $service;
+
+    public function __construct(RecipeService $service)
+    {
+        $this->service = $service;
+    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Modules\Users\Services\UserService;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    protected $service;
+
+    public function __construct(UserService $service)
+    {
+        $this->service = $service;
+    }
 }
