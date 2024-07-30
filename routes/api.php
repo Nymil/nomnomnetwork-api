1b3fa3b3c96a910a64041743be7e4d857544f99f
Mattice Heyzak <mattice.heyzak@student.howest.be>
2024-07-30 11:29:10 +0200

    post to patch where it was get to avoid dupplicates

--- routes/api.php
+++ routes/api.php
@@ -18,25 +18,25 @@ use Illuminate\Support\Facades\Route;
 Route::post("users/login", [UserController::class, 'login']);
 Route::post("users/register", [UserController::class, 'add']);
 
+Route::get("images/{image_name}", [RecipeController::class, 'getImage']);
+
 Route::group([
     "middleware" => ["auth:api"]
 ], function () {
-    Route::post("users/check", function () { return response()->json(["message" => "User is still logged in"]); });
-    Route::post("users/logout", [UserController::class, 'logout']);
-    Route::post("users/refresh", [UserController::class, 'refresh']);
+    Route::patch("users/check", function () { return response()->json(["message" => "User is still logged in"]); });
+    Route::patch("users/logout", [UserController::class, 'logout']);
+    Route::patch("users/refresh", [UserController::class, 'refresh']);
 
     Route::put("users/{user_id}/recipes/{recipe_id}", [UserController::class, 'likeRecipe']); // for liking a recipe
     Route::delete("users/{user_id}/recipes/{recipe_id}", [UserController::class, 'unlikeRecipe']); // for unliking a recipe
 
-    Route::post("recipes", [RecipeController::class, 'all']);
-    Route::post("recipes/{id}", [RecipeController::class, 'get']);
+    Route::patch("recipes", [RecipeController::class, 'all']);
+    Route::patch("recipes/{id}", [RecipeController::class, 'get']);
     Route::post("recipes", [RecipeController::class, 'add']);
 
-    Route::post("recipes/liked/{user_id}", [UserController::class, 'getLikedRecipes']);
-    Route::post("recipes/created/{user_id}", [UserController::class, 'getCreatedRecipes']);
+    Route::patch("recipes/liked/{user_id}", [UserController::class, 'getLikedRecipes']);
+    Route::patch("recipes/created/{user_id}", [UserController::class, 'getCreatedRecipes']);
 
     Route::delete("recipes/{id}", [RecipeController::class, 'delete']);
     Route::put("recipes/{id}", function () { return "Not implemented"; });
-
-    Route::post("images/{image_name}", [RecipeController::class, 'getImage']);
 });
